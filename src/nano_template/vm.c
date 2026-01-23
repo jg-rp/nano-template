47a4059d0c2a613e3c649782dce70ebf5e9a4e0c
James Prior <jamesgr.prior@gmail.com>
2026-01-23 18:09:32 +0000

    VM WIP [skip ci]

--- src/nano_template/vm.c
+++ src/nano_template/vm.c
@@ -46,12 +46,31 @@ static OpFn vm_op_table[] = {
     [NT_OP_FALSE] = vm_op_false,
 };
 
+/// @brief Read a single operand at offset `offset`.
+static int vm_read_operand(NT_VM *vm, uint8_t byte_count, size_t offset);
+
+/// @brief Write `obj` to the output buffer.
+static int vm_render(NT_VM *vm, PyObject *obj);
+
+/// @brief Put `obj` on top of the stack and create a new reference to it.
 static int vm_push(NT_VM *vm, PyObject *obj);
+
+/// @brief Pop the top off the stack and return a reference owned by the
+/// caller.
+/// @return A pointer to the popped object, or NULL if the top of the stack was
+/// empty with an exception set.
 static PyObject *vm_pop(NT_VM *vm);
+
+/// @brief Return a borrowed reference to stack top.
 static PyObject *vm_peek(NT_VM *vm);
-static size_t *vm_current_frame(NT_VM *vm);
-static void *vm_push_frame(NT_VM *vm, size_t frame);
-static size_t *vm_pop_frame(NT_VM *vm);
+
+/// @brief Get an iterator for `obj`.
+/// @return 0 on success, -1 on error with an exception set.
+static int vm_iter(PyObject *obj, PyObject **out_iter);
+
+static size_t vm_current_frame(NT_VM *vm);
+static int vm_push_frame(NT_VM *vm, size_t frame);
+static size_t vm_pop_frame(NT_VM *vm);
 
 NT_VM *NT_VM_new(NT_Code *code, PyObject *serializer, PyObject *undefined)
 {
@@ -198,7 +217,10 @@ int NT_VM_run(NT_VM *vm, PyObject *data)
             return NULL;
         }
 
-        fn(vm, &ip, data);
+        if (fn(vm, &ip, data) < 0)
+        {
+            return -1;
+        }
     }
 }
 
@@ -207,4 +229,404 @@ PyObject *NT_VM_join(NT_VM *vm)
     PyObject *result = StringBuffer_finish(vm->buf);
     vm->buf = NULL;
     return result;
+}
+
+int vm_read_operand(NT_VM *vm, uint8_t byte_count, size_t offset)
+{
+    assert(offset + byte_count - 1 < vm->instructions_size);
+
+    int value = 0;
+    for (int i = 0; i < byte_count; i++)
+    {
+        value = (value << 8) | vm->instructions[offset + i];
+    }
+    return value;
+}
+
+static int vm_render(NT_VM *vm, PyObject *obj)
+{
+    PyObject *str = PyObject_CallFunctionObjArgs(vm->serializer, obj, NULL);
+
+    if (!str)
+    {
+        return -1;
+    }
+
+    if (StringBuffer_append(vm->buf, str) < 0)
+    {
+        Py_DECREF(str);
+        return -1;
+    }
+
+    Py_DECREF(str);
+    return 0;
+}
+
+static int vm_push(NT_VM *vm, PyObject *obj)
+{
+    vm->stack[vm->sp++] = obj;
+    Py_INCREF(obj);
+    // TODO: check for stack overflow
+    return 0;
+}
+
+static PyObject *vm_pop(NT_VM *vm)
+{
+    vm->sp--;
+
+    PyObject *obj = vm->stack[vm->sp];
+    if (!obj)
+    {
+        PyErr_SetString(PyExc_RuntimeError, "unexpected pop of null pointer");
+    }
+
+    vm->stack[vm->sp] = NULL;
+    return obj;
+}
+
+static PyObject *vm_peek(NT_VM *vm)
+{
+    vm->stack[vm->sp - 1];
+}
+
+static size_t vm_current_frame(NT_VM *vm)
+{
+    return vm->frames[vm->fp];
+}
+
+static int vm_push_frame(NT_VM *vm, size_t frame)
+{
+    vm->frames[vm->fp++] = frame;
+    // TODO: check for frame overflow.
+    return 0;
+}
+
+static size_t vm_pop_frame(NT_VM *vm)
+{
+    return vm->frames[--vm->fp];
+}
+
+static int vm_noop(NT_VM *vm, size_t *ip, PyObject *data)
+{
+    return 0;
+}
+
+static int vm_op_text(NT_VM *vm, size_t *ip, PyObject *data)
+{
+    int constant_index = vm_read_operand(vm, 2, *ip + 1);
+
+    if (vm_render(vm, vm->constant_pool[constant_index]) < 0)
+    {
+        return -1;
+    }
+
+    *ip += 3;
+    return 0;
+}
+
+static int vm_op_render(NT_VM *vm, size_t *ip, PyObject *data)
+{
+    PyObject *obj = vm_pop(vm);
+    if (!obj)
+    {
+        return -1;
+    }
+
+    if (vm_render(vm, obj) < 0)
+    {
+        return -1;
+    }
+
+    *ip += 1;
+    return 0;
+}
+
+static int vm_op_not(NT_VM *vm, size_t *ip, PyObject *data)
+{
+    PyObject *obj = vm_pop(vm);
+    if (!obj)
+    {
+        return -1;
+    }
+
+    int not_obj = PyObject_IsFalse(obj);
+    Py_DECREF(obj);
+
+    if (vm_push(vm, not_obj ? Py_True : Py_False) < 0)
+    {
+        return -1;
+    }
+
+    *ip += 1;
+    return 0;
+}
+
+static int vm_op_constant(NT_VM *vm, size_t *ip, PyObject *data)
+{
+    int constant_index = vm_read_operand(vm, 2, *ip + 1);
+
+    if (vm_push(vm, vm->constant_pool[constant_index]) < 0)
+    {
+        return -1;
+    }
+
+    *ip += 3;
+    return 0;
+}
+
+static int vm_op_global(NT_VM *vm, size_t *ip, PyObject *data)
+{
+    int constant_index = vm_read_operand(vm, 2, *ip + 1);
+
+    PyObject *obj = PyObject_GetItem(data, vm->constant_pool[constant_index]);
+    if (!obj)
+    {
+        // TODO: undefined constructor.
+        NTPY_TODO_I();
+    }
+
+    if (vm_push(vm, obj) < 0)
+    {
+        return -1;
+    }
+
+    *ip += 3;
+    return 0;
+}
+
+static int vm_op_selector(NT_VM *vm, size_t *ip, PyObject *data)
+{
+    int constant_index = vm_read_operand(vm, 2, *ip + 1);
+
+    PyObject *obj = vm_pop(vm);
+    if (!obj)
+    {
+        return -1;
+    }
+
+    Py_DECREF(obj);
+
+    PyObject *new_obj = PyObject_GetItem(data, obj);
+    if (!obj)
+    {
+        // TODO: undefined constructor.
+        NTPY_TODO_I();
+    }
+
+    if (vm_push(vm, obj) < 0)
+    {
+        return -1;
+    }
+
+    *ip += 3;
+    return 0;
+}
+
+static int vm_op_pop(NT_VM *vm, size_t *ip, PyObject *data)
+{
+    int result = vm_pop(vm);
+    *ip++;
+    return result;
+}
+
+static int vm_op_jump(NT_VM *vm, size_t *ip, PyObject *data)
+{
+    *ip = vm_read_operand(vm, 2, *ip + 1);
+    return 0;
+}
+
+static int vm_op_jump_if_falsy(NT_VM *vm, size_t *ip, PyObject *data)
+{
+    size_t jump_pos = vm_read_operand(vm, 2, *ip + 1);
+    PyObject *obj = vm_peek(vm);
+    *ip = PyObject_IsFalse(obj) ? jump_pos : ip + 3;
+    return 0;
+}
+
+static int vm_op_jump_if_truthy(NT_VM *vm, size_t *ip, PyObject *data)
+{
+    size_t jump_pos = vm_read_operand(vm, 2, *ip + 1);
+    PyObject *obj = vm_peek(vm);
+    *ip = PyObject_IsTrue(obj) ? jump_pos : ip + 3;
+    return 0;
+}
+
+static int vm_op_set_local(NT_VM *vm, size_t *ip, PyObject *data)
+{
+    size_t local_index = vm_read_operand(vm, 1, *ip + 1);
+
+    PyObject *obj = vm_pop(vm);
+    if (!obj)
+    {
+        return -1;
+    }
+
+    // Don't decref obj as we're inserting it back into the stack.
+    vm->stack[vm_current_frame(vm) + local_index] = obj;
+    *ip += 2;
+    return 0;
+}
+
+static int vm_op_get_local(NT_VM *vm, size_t *ip, PyObject *data)
+{
+    size_t depth = vm_read_operand(vm, 1, *ip + 1);
+    size_t offset = vm_read_operand(vm, 1, *ip + 2);
+
+    size_t stack_pointer = vm->frames[vm->fp - 1 - depth] + offset;
+    if (vm_push(vm, vm->stack[stack_pointer]) < 0)
+    {
+        return -1;
+    }
+
+    *ip += 3;
+    return 0;
+}
+
+static int vm_op_iter_init(NT_VM *vm, size_t *ip, PyObject *data)
+{
+    PyObject *it = NULL;
+
+    PyObject *obj = vm_pop(vm);
+    if (!obj)
+    {
+        return -1;
+    }
+
+    if (vm_iter(obj, &it) < 0)
+    {
+        Py_DECREF(obj);
+        return -1;
+    }
+
+    Py_DECREF(obj);
+
+    if (vm_push(vm, it) < 0)
+    {
+        Py_DECREF(it);
+        return -1;
+    }
+
+    Py_DECREF(it);
+
+    *ip++;
+    return 0;
+}
+
+static int vm_op_iter_next(NT_VM *vm, size_t *ip, PyObject *data)
+{
+    PyObject *it = vm->stack[vm->sp - 1];
+
+    PyObject *value = PyIter_Next(it);
+    if (!value)
+    {
+        if (PyErr_Occurred())
+        {
+            return -1;
+        }
+
+        if (vm_push(vm, Py_False) < 0)
+        {
+            return -1;
+        }
+    }
+
+    if (vm_push(vm, value) < 0)
+    {
+        Py_DECREF(value);
+        return -1;
+    }
+
+    Py_DECREF(value);
+
+    if (vm_push(vm, Py_True) < 0)
+    {
+        return -1;
+    }
+
+    return 0;
+}
+
+static int vm_op_enter_frame(NT_VM *vm, size_t *ip, PyObject *data)
+{
+    int n_locals = vm_read_operand(vm, 1, vm->sp + 1);
+
+    if (vm_push_frame(vm, vm->sp) < 0)
+    {
+        return -1;
+    }
+
+    vm->sp += n_locals;
+    *ip += 2;
+}
+
+static int vm_op_leave_frame(NT_VM *vm, size_t *ip, PyObject *data)
+{
+    size_t frame = vm_pop_frame(vm);
+    vm->sp = frame;
+    *ip++;
+    return 0;
+}
+
+static int vm_op_true(NT_VM *vm, size_t *ip, PyObject *data)
+{
+    if (vm_push(vm, Py_True) < 0)
+    {
+        return -1;
+    }
+
+    *ip++;
+    return 0;
+}
+
+static int vm_op_false(NT_VM *vm, size_t *ip, PyObject *data)
+{
+    if (vm_push(vm, Py_False) < 0)
+    {
+        return -1;
+    }
+
+    *ip++;
+    return 0;
+}
+
+static int vm_iter(PyObject *op, PyObject **out_iter)
+{
+    PyObject *it = NULL;
+    *out_iter = NULL;
+
+    PyObject *items = PyMapping_Items(op);
+
+    if (items)
+    {
+        it = PyObject_GetIter(items);
+        Py_DECREF(items);
+
+        if (!it)
+        {
+            return -1;
+        }
+
+        *out_iter = it;
+        return 0;
+    }
+
+    if (PyErr_Occurred())
+    {
+        PyErr_Clear();
+    }
+
+    it = PyObject_GetIter(op);
+    if (it)
+    {
+        *out_iter = it;
+        return 0;
+    }
+
+    if (PyErr_Occurred())
+    {
+        PyErr_Clear();
+    }
+
+    *out_iter = PyList_New(0);
+    return 0;
 }
